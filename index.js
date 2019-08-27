1d719c56a82e1de9d8dced1ac134b0a398ea6357
Acer <john.kosinski@gmail.com>
2019-08-27 08:33:52 +0700

    added mock db for movies

--- index.js
+++ index.js
@@ -4,12 +4,13 @@
 const config = require('./lib/config');
 if (!config.isProd()) {
     require('dotenv').config();
-}   
+}
 
 //configure IOC container
 const ioc = require('./lib/iocContainer');
 ioc.service('loggerFactory', c => require('./lib/winstonLogger'));
 ioc.service('ehFactory', c => require('./lib/exceptionHandler'));
+ioc.service('database', c => require('./lib/data/mockdb'));
 
 
 const server = require('./lib/server');
--- test/test.js
+++ test/test.js
@@ -2,13 +2,18 @@
 
 //configure IOC container
 const ioc = require('../lib/iocContainer');
-ioc.service('loggerFactory', c => require('../lib/winstonLogger')); 
-ioc.service('ehFactory', c => require('../lib/exceptionHandler')); 
+ioc.service('loggerFactory', c => require('../lib/winstonLogger'));
+ioc.service('ehFactory', c => require('../lib/exceptionHandler'));
+ioc.service('database', c => require('../lib/data/mockdb'));
 
 const expect  = require('chai').expect;
 
 describe('Movie Tests', async () => {
     it ('get movies', async () => {
-		
+        await(database.initialize());
+        const movies = await database.getMovies();
+
+        expect(movies, 'movies output is null').to.exist;
+        expect(movies.length).to.not.equal(0, 'movies.length is 0');
     });
 });
